7624beecc6c11f2b94a4b45c0a713320e6630709
reymart-vi <110107868+reymart-vi@users.noreply.github.com>
2023-08-26 17:20:53 +0800

    - quick fix

--- api/index.js
+++ api/index.js
@@ -22,15 +22,15 @@ const app = express();
 // app.use(fileUpload());
 // app.use(_bodyParser.urlencoded({ extended: true }));
 // app.use(_bodyParser.json());
-app.use(express.static(_path.join(__dirname, 'public')));
-app.use(express.static(_path.join(__dirname, 'views')));
+app.use(express.static(_path.join(__dirname, '../public')));
+app.use(express.static(_path.join(__dirname, '../views')));
 app.set("view engine", "ejs",);
-app.set("views", [_path.join(__dirname, 'views'), _path.join(__dirname, 'views/home'), _path.join(__dirname, 'views/login-system'), _path.join(__dirname, 'views/navigation'), _path.join(__dirname, 'views/sys-users'),_path.join(__dirname, 'views/audit-log')]);
+app.set("views", [_path.join(__dirname, '../views'), _path.join(__dirname, '../views/home'), _path.join(__dirname, '../views/login-system'), _path.join(__dirname, '../views/navigation'), _path.join(__dirname, '../views/sys-users'),_path.join(__dirname, '../views/audit-log')]);
 app.use(express.static(__dirname + "/public"));
 app.use(flash());
 app.use((req, res, next)=> { res.locals.messages = require("express-messages")(req, res); next();});
 app.listen(process.env.PORT , () => console.log("LISTENING TO PORT : "+process.env.PORT));
 app.use(function(err, req, res, next) { res.status(err.status || 500); res.send(err);});
 
-const _home = require('./routes/home')
+const _home = require('../routes/home')
 app.use('/', _home);
